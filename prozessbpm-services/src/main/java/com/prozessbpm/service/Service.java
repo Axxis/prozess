b64fed8f677fe49279e8a3bd24b2a70b278169dc
Otto Giron <ottog2486@gmail.com>
2013-06-17 19:29:38 -0700

    ProzessBPM:First projects commit

---
+++ prozessbpm-services/src/main/java/com/prozessbpm/service/Service.java
@@ -0,0 +1,7 @@
+package com.prozessbpm.service;
+
+public interface Service {
+	
+	String getMessage();
+
+}
---
+++ prozessbpm-services/src/test/java/com/prozessbpm/service/ExampleServiceTests.java
@@ -0,0 +1,14 @@
+package com.prozessbpm.service;
+
+import com.prozessbpm.service.ExampleService;
+import junit.framework.TestCase;
+
+public class ExampleServiceTests extends TestCase {
+
+	private ExampleService service = new ExampleService();
+	
+	public void testReadOnce() throws Exception {
+		assertEquals("Hello world!", service.getMessage());
+	}
+
+}
