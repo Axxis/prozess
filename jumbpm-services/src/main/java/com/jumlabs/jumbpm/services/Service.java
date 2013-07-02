8e72239b963349776fb9b59c67b041c666429387
Otto Giron <ottog2486@gmail.com>
2013-07-01 18:24:46 -0600

    Total refactor of project name and references

---
+++ jumbpm-services/src/main/java/com/jumlabs/jumbpm/services/Service.java
@@ -0,0 +1,7 @@
+package com.jumlabs.jumbpm.services;
+
+public interface Service {
+	
+	String getMessage();
+
+}
--- prozessbpm-services/src/main/java/com/prozessbpm/service/Service.java
+++
@@ -1,7 +0,0 @@
-package com.prozessbpm.service;
-
-public interface Service {
-	
-	String getMessage();
-
-}
